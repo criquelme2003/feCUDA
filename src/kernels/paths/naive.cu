322cef940891e304a7ddd405d1f8ba8e7c09efe8
criquelme2003 <carlitisamuel@gmail.com>
2026-08-28 16:32:37 -0400

    fix: failed paths

--- src/kernels/paths/naive.cu
+++
@@ -1,8 +0,0 @@
-
-/*
-Busqueda naive de coindicencias entre 
-    A[0,i + 1] y B[0,1]
-Siento A y B matrices bidimencionales de P1 x (3+i) y P2 x 3
-Donde a cada hilo se le asigna un valor de B[0,1] (distinto para cada hilo), y este busca entre todos los posibles valores de 
-*/
-
